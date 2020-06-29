58fead322e9c86409dcbc4201f0f94ffd1ab0911
klementev <assssseeeee@gmail.com>
2020-06-29 14:48:54 +0300

    new commit

--- app/src/main/java/com/example/moneytracker/data/DatabaseHelper.java
+++ app/src/main/java/com/example/moneytracker/data/DatabaseHelper.java
@@ -1,13 +1,13 @@
 package com.example.moneytracker.data;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
-
 import com.example.moneytracker.data.MoneyTrackerContract.AddingExpenses;
-
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -39,14 +39,31 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public void loadDatabaseFromFile(){
+    public void loadDatabaseFromFile() {
         InputStream inputStream = null;
         OutputStream outputStream = null;
-        try{
+        try {
             File file = new File(MoneyTrackerContract.DATA_BASE_PATH);
-            if()
-        }catch (IOException ex){
+            if (!file.exists()) {
+                this.getReadableDatabase();
+                inputStream = myContext.getAssets().open(MoneyTrackerContract.DATA_BASE_NAME);
+                String outFileName = MoneyTrackerContract.DATA_BASE_PATH;
+                outputStream = new FileOutputStream(outFileName);
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = inputStream.read(buffer)) > 0) {
+                    outputStream.write(buffer, 0, length);
+                }
+                outputStream.flush();
+                outputStream.close();
+                inputStream.close();
+            }
+        } catch (IOException ex) {
             Log.d("DatabaseHelper", ex.getMessage());
         }
+    }
+
+    public SQLiteDatabase open() throws SQLException {
+        return SQLiteDatabase.openDatabase(MoneyTrackerContract.DATA_BASE_PATH, null, SQLiteDatabase.OPEN_READWRITE);
     }
 }
