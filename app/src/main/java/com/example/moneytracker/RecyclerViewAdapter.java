bcb2434bdffb3a1751a4e5fe92bbaa796ff4db89
klementev <assssseeeee@gmail.com>
2021-04-21 23:29:53 +0300

    work to RecyclerViewAdapter and Binding

--- app/src/main/java/com/example/moneytracker/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/moneytracker/RecyclerViewAdapter.java
@@ -6,13 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.moneytracker.data.MoneyTrackerContract;
-
-import java.util.ArrayList;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewViewHolder> {
     Context context;
